92d89e81648ad3195fb72b76b6cf8aab7accbd54
jmh3434 <jmh3434@gmail.com>
2018-05-24 13:20:59 -0400

    Initial Commit

---
+++ lithi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lithi
+//
+//  Created by James Hunt on 5/24/18.
+//  Copyright © 2018 James Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
